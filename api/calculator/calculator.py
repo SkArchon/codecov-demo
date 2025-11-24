f5a1c5948d6798a6f76eddbffefb7443be51aa10
Milinda Dias <skay.archon@gmail.com>
2025-11-24 21:18:25 +0530

    update

--- api/calculator/calculator.py
+++ api/calculator/calculator.py
@@ -18,83 +18,83 @@ class Calculator:
             return 'Cannot divide by 0'
         return x * 1.0 / y
 
-    def divide2(x, y):
-        if y == 0:
-            return 'Cannot divide by 0'
-        return x * 1.0 / y
+    # def divide2(x, y):
+    #     if y == 0:
+    #         return 'Cannot divide by 0'
+    #     return x * 1.0 / y
 
-    def divide3(x, y):
-        if y == 0:
-            return 'Cannot divide by 0'
-        return x * 1.0 / y
+    # def divide3(x, y):
+    #     if y == 0:
+    #         return 'Cannot divide by 0'
+    #     return x * 1.0 / y
 
-    def divide4(x, y):
-        if y == 0:
-            return 'Cannot divide by 0'
-        return x * 1.0 / y
+    # def divide4(x, y):
+    #     if y == 0:
+    #         return 'Cannot divide by 0'
+    #     return x * 1.0 / y
 
-    def divide5(x, y):
-        if y == 0:
-            return 'Cannot divide by 0'
-        return x * 1.0 / y
+    # def divide5(x, y):
+    #     if y == 0:
+    #         return 'Cannot divide by 0'
+    #     return x * 1.0 / y
 
-    def divide6(x, y):
-        if y == 0:
-            return 'Cannot divide by 0'
-        return x * 1.0 / y
+    # def divide6(x, y):
+    #     if y == 0:
+    #         return 'Cannot divide by 0'
+    #     return x * 1.0 / y
 
-    def divide7(x, y):
-        if y == 0:
-            return 'Cannot divide by 0'
-        return x * 1.0 / y
+    # def divide7(x, y):
+    #     if y == 0:
+    #         return 'Cannot divide by 0'
+    #     return x * 1.0 / y
 
-    def divide8(x, y):
-        if y == 0:
-            return 'Cannot divide by 0'
-        return x * 1.0 / y
+    # def divide8(x, y):
+    #     if y == 0:
+    #         return 'Cannot divide by 0'
+    #     return x * 1.0 / y
 
-    def divide9(x, y):
-        if y == 0:
-            return 'Cannot divide by 0'
-        return x * 1.0 / y
+    # def divide9(x, y):
+    #     if y == 0:
+    #         return 'Cannot divide by 0'
+    #     return x * 1.0 / y
 
-    def divide10(x, y):
-        if y == 0:
-            return 'Cannot divide by 0'
-        return x * 1.0 / y
+    # def divide10(x, y):
+    #     if y == 0:
+    #         return 'Cannot divide by 0'
+    #     return x * 1.0 / y
 
-    def divide11(x, y):
-        if y == 0:
-            return 'Cannot divide by 0'
-        return x * 1.0 / y
+    # def divide11(x, y):
+    #     if y == 0:
+    #         return 'Cannot divide by 0'
+    #     return x * 1.0 / y
 
-    def divide12(x, y):
-        if y == 0:
-            return 'Cannot divide by 0'
-        return x * 1.0 / y
+    # def divide12(x, y):
+    #     if y == 0:
+    #         return 'Cannot divide by 0'
+    #     return x * 1.0 / y
 
-    def divide13(x, y):
-        if y == 0:
-            return 'Cannot divide by 0'
-        return x * 1.0 / y
+    # def divide13(x, y):
+    #     if y == 0:
+    #         return 'Cannot divide by 0'
+    #     return x * 1.0 / y
 
-    def divide14(x, y):
-        if y == 0:
-            return 'Cannot divide by 0'
-        return x * 1.0 / y
+    # def divide14(x, y):
+    #     if y == 0:
+    #         return 'Cannot divide by 0'
+    #     return x * 1.0 / y
 
-    def divide15(x, y):
-        if y == 0:
-            return 'Cannot divide by 0' 
-        return x * 1.0 / y
+    # def divide15(x, y):
+    #     if y == 0:
+    #         return 'Cannot divide by 0' 
+    #     return x * 1.0 / y
 
-    def divide16(x, y):
-        if y == 0:
-            return 'Cannot divide by 0'
-        return x * 1.0 / y
+    # def divide16(x, y):
+    #     if y == 0:
+    #         return 'Cannot divide by 0'
+    #     return x * 1.0 / y
 
-    def divide17(x, y):
-        if y == 0:
-            return 'Cannot divide by 0'
-        return x * 1.0 / y
+    # def divide17(x, y):
+    #     if y == 0:
+    #         return 'Cannot divide by 0'
+    #     return x * 1.0 / y
         
