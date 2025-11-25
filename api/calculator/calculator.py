1d1a1d0f131f40a316aaf8fb62f768ee1c9e97a5
Milinda Dias <skay.archon@gmail.com>
2025-11-25 17:39:35 +0530

    a

--- api/calculator/calculator.py
+++ api/calculator/calculator.py
@@ -8,10 +8,10 @@ class Calculator:
     def multiply(x, y):
         return x * y
 
-    def divide(x, y):
-        # if y == 0:
-        #     return 'Cannot divide by 0'
-        return x * 1.0 / y
+    # def divide(x, y):
+    #     # if y == 0:
+    #     #     return 'Cannot divide by 0'
+    #     return x * 1.0 / y
 
     # def divide1(x, y):
     #     if y == 0:
